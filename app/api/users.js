ccef33be4064904f947a598fcc80a64c9820ad1a
Wagner Perin <wagnerperin@gmail.com>
2018-06-20 14:24:21 -0300

    Validating request body content

--- app/api/users.js
+++ app/api/users.js
@@ -2,15 +2,39 @@ module.exports = app => {
     const userModel = require('mongoose').model('User');
 
     const api = {};
+    const error = app.errors.users;
 
     api.create = (req, res) => {
-        userModel
-            .create(req.body)
-            .then(user => {
-                res.status(200).json(user);
-            }, error => {
-                console.log(error);
-            })
+        console.log(req.body);
+        if(Array.isArray(req.body) || (req.body.constructor === Object && Object.keys(req.body).length === 0)) res.status(400).json(error.parse('users-1', new Error('This resource spect a JSON user object in the body request.')));
+        else{
+            userModel
+                .create(req.body)
+                .then(user => {
+                    user.link = {
+                        rel: 'self',
+                        href: app.get('usersApiRoute') + user._id
+                    };
+                    user.save();
+                    user = user.toObject();
+                    delete user.password;
+                    res.status(201).json({
+                        userMessage: 'User created success',
+                        user
+                    })
+                }, err => {
+                    switch(err.name) {
+                        case 'ValidationError':
+                            res.status(400).json(error.parse('users-2', err));
+                            break;
+                        case 'MongoError':
+                            res.status(400).json(error.parse('users-3', err));
+                            break;
+                        default:
+                            res.status(500).json(error.parse('users-4', err));
+                    }
+                });  
+        }
     }
 
     return api;
