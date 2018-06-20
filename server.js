8fc9e44f2715343977e5850cb2d0a3a8bf99ea52
Wagner Perin <wagnerperin@gmail.com>
2018-06-20 13:15:05 -0300

    establishing connections with the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./config/express');
+require('./config/database')(app.get('dbUri'));
 
 http.createServer(app).listen(app.get('port'), () => {
     console.log('Server running at port ' + app.get('port'));
