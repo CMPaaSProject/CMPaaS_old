27391f74c7f67d8d29f8e0ae60bccacf713a8cbc
Wagner Perin <wagnerperin@gmail.com>
2018-06-20 12:35:13 -0300

    Initial server configuration

---
+++ config/express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+//Application Deffinitions
+app.set('port', 3000);
+
+//Middlewares Settups
+app.use(morgan('dev'));
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+
+//Consign load configuration
+consign({cwd: 'app'})
+    .include('errors')
+    .then('models')
+    .then('api')
+    .then('routes')
+    .into(app);
+
+module.exports = app;
